e4b27f88a43d57e6aaaee21f75c403a100c68301
Levir Cesar <levirlemos@gmail.com>
2021-04-20 11:56:48 -0300

    segundo dia finalizado

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+//import de api
+//SPA - useEffect (nao indexa no google)
+{/*
+useEffect(()=>{
+    fetch('http://localhost:3333/episodes')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  },[]);
+*/}
+//SSR - getServerSideProps
+//SSG - getStaticProps - so funciona em producao
+
+export default function Home(props) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  )
+}
+
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 *8,
+  } 
+}
