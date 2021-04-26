53a61a7dbd283dd47c70c27a00948f9f7cdffd83
Levir Cesar <levirlemos@gmail.com>
2021-04-26 14:43:49 -0300

    mudei api.ts e index.tsx teste de api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://vercel-api-phi.vercel.app/api/'
+  baseURL: 'https://testeserverproffy.herokuapp.com/'
   //'http://localhost:3333/'
   //'https://vercel-api-phi.vercel.app/api/'
   //'https://testeserverproffy.herokuapp.com/'
