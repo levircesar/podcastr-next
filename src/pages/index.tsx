220f93e7e73606f44d1cf7b5583da30889551c13
Levir Cesar <levirlemos@gmail.com>
2021-04-26 14:31:52 -0300

    teste de api3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -152,6 +152,6 @@ export const getStaticProps: GetStaticProps = async  () => {
       latestEpisodes,
       allEpisodes,
     },
-    revalidate: 60 ,
+    revalidate: 60 * 60 *8,
   } 
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://testeserverproffy.herokuapp.com/'
+  baseURL: 'https://vercel-api-phi.vercel.app/api/'
   //'http://localhost:3333/'
   //'https://vercel-api-phi.vercel.app/api/'
   //'https://testeserverproffy.herokuapp.com/'
